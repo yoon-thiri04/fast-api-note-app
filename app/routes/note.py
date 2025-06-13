ba1f2f9ff8dd4b9b760cbd92dc95315267f48199
Yoon Thiri Aung <yoonthiriaung04@gmail.com>
2025-06-13 09:12:54 +0630

    delete and update notes

--- app/routes/note.py
+++ app/routes/note.py
@@ -25,10 +25,21 @@ async def get_notes_api( current_user=Depends(get_current_user)):
     notes =await get_notes(current_user["id"])
     return  [NoteResponse.from_mongo(note) for note in notes]
 
-@router.put("/{note_id}")
-async def update_note_api(note_id:str):
-    pass
+@router.put("/{note_id}",response_model=dict)
+async def update_note_api(note_id:str,note:NoteUpdate,current_user=Depends(get_current_user)):
 
-@router.delete("/{noted_id}")
-async def delete_note_api(noted_id:str):
-    pass
+    
+    updated = await update_note(note_id,note, current_user["id"])
+
+    if not updated:
+        raise HTTPException(status_code=404, detail="Note not found!")
+    
+    return {"message":"note updated"}
+@router.delete("/{note_id}",response_model=dict)
+async def delete_note_api(note_id:str,current_user=Depends(get_current_user)):
+    print(note_id)
+    delected = await delete_note(note_id)
+    if not delected:
+        raise HTTPException(status_code=404, detail="Note not found")
+    
+    return {"message":"Note delected"}
