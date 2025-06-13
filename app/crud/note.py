df0b4ba136de09e311820be554cd590cf243348c
Yoon Thiri Aung <yoonthiriaung04@gmail.com>
2025-06-13 09:12:36 +0630

    update & delete note

--- app/crud/note.py
+++ app/crud/note.py
@@ -22,8 +22,24 @@ async def get_notes(user_id:str):
     
     return notes
 
-async def update_note(noted_id : str, note:NoteUpdate):
-    pass
+async def update_note(note_id: str, note: NoteUpdate, user_id: str):
+    update_fields = note.model_dump(exclude_unset=True)
+    update_fields["user_id"] = user_id
 
-async def delete_note(noted_id: str):
-    pass
+    result = await notes_collection.update_one(
+        {"_id": ObjectId(note_id)},
+        {"$set": update_fields}
+    )
+
+    if result.modified_count == 0:
+        return False
+    return True
+
+async def delete_note(note_id: str):
+    print(note_id)
+    result=await notes_collection.delete_one(
+        {"_id":ObjectId(note_id)}
+    )
+    if result.deleted_count == 0:
+        return False
+    return True
